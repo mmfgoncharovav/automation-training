29e19ff98d6bde713198ee24a6874cdf38948dcb
Alexandr <goncharovavmmf@gmail.com>
2020-01-03 01:44:47 +0300

    changes for new version of page

--- webdriver/src/test/java/by/bsu/rent/driver/DriverSingleton.java
+++ webdriver/src/test/java/by/bsu/rent/driver/DriverSingleton.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class DriverSingleton {
     private static WebDriver driver;
 
@@ -14,7 +16,8 @@ public class DriverSingleton {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     driver.manage().window().maximize();
-        return driver;
+                    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                    return driver;
     }
     public static void closeDriver() {
         driver.quit();
